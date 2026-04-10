7ad24f423cd4c6cd8972aa2c8ca9a880c3de059e
vishuRizz <vishurizz0@gmail.com>
2026-04-10 18:56:51 +0530

    coming soon to live

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -2,6 +2,6 @@ export const BASE_URL = "https://api.admin.lufasilodges.com";
 // export const BASE_URL = "http://localhost:4000";
 
 // Toggle this between "live" and "coming-soon"
-export const SITE_MODE = "coming-soon";
+export const SITE_MODE = "live";
 
 export const isComingSoonEnabled = () => SITE_MODE === "coming-soon";
